c709343b228def27ed3d31385b69da5ba469da69
Manu63 <81447790+Manu63x@users.noreply.github.com>
2022-09-22 11:40:15 +0200

    Auto and Manual Carousel

--- script.js
+++ script.js
@@ -1,13 +1,30 @@
 let slideIndex = 1;
 showSlides(slideIndex);
 
+window.addEventListener("load",function() {
+  showSlides(slideIndex);
+  myTimer = setInterval(function(){plusSlides(1)}, 4000);
+})
+
 // Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
+function plusSlides(n){
+  clearInterval(myTimer);
+  if (n < 0){
+    showSlides(slideIndex -= 1);
+  } else {
+   showSlides(slideIndex += 1); 
+  }
+  if (n === -1){
+    myTimer = setInterval(function(){plusSlides(n + 2)}, 4000);
+  } else {
+    myTimer = setInterval(function(){plusSlides(n + 1)}, 4000);
+  }
 }
 
 // Thumbnail image controls
-function currentSlide(n) {
+function currentSlide(n){
+  clearInterval(myTimer);
+  myTimer = setInterval(function(){plusSlides(n + 1)}, 4000);
   showSlides(slideIndex = n);
 }
 
